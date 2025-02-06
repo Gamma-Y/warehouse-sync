64dcbb8291f6d4f3f35cb5ee4f6b41f1b8c209c6
Evgeny Smirnov <HoUsToN.97@mail.ru>
2025-02-06 22:36:58 +0500

    init project

---
+++ src/main/java/ru/pet/proejct/warehousesync/WarehouseSyncApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.proejct.warehousesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pet/proejct/warehousesync/WarehouseSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.proejct.warehousesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
